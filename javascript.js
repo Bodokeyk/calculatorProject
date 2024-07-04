57a76f73cc0a81bb05a7fb94aa32ca57355cee82
bobodokey <94241624+Bodokeyk@users.noreply.github.com>
2024-07-03 19:19:31 -0600

    add basic arithmethic functions and variables for calculator

---
+++ javascript.js
@@ -0,0 +1,16 @@
+/* Start with basic functions */
+const valueOne = 2;
+const ValueTwo = 3;
+const operatorOn = '+'
+function sum(firstValue, SecondValue){
+return firstValue + SecondValue
+}
+function subs(firstValue, SecondValue){
+return firstValue - SecondValue
+    }
+function mult(firstValue, SecondValue){
+return firstValue * SecondValue
+}
+function div(firstValue, SecondValue){
+return firstValue / SecondValue
+}
