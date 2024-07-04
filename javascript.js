8bb04700fd7b6ed699b85c542c5983798ebe593a
bobodokey <94241624+Bodokeyk@users.noreply.github.com>
2024-07-04 16:45:08 -0600

    get basic operate function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 /* Start with basic functions */
 const valueOne = 2;
 const ValueTwo = 3;
-const operatorOn = '+'
+const operatorOn = '/'
 function sum(firstValue, SecondValue){
 return firstValue + SecondValue
 }
@@ -14,3 +14,16 @@ return firstValue * SecondValue
 function div(firstValue, SecondValue){
 return firstValue / SecondValue
 }
+/* Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers. */
+function operate(oneTOperate, operator, twoTOperate){
+ switch(operator){
+    case "+": return sum(oneTOperate, twoTOperate);
+    case "-": return subs(oneTOperate, twoTOperate);
+    case "*": return mult(oneTOperate,twoTOperate);
+    case "/": return div(oneTOperate,  twoTOperate);
+    default:
+        console.log("ERROR")
+        break
+}
+}
+console.log(operate(valueOne,operatorOn, ValueTwo))
